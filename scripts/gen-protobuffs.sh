df43b995f749196b5fdd0a9459e3305cf7f29906
Shikanime <deva.shikanime@protonmail.com>
2020-02-25 23:44:58 +0100

    Lint scripts

--- scripts/gen-protobuffs.sh
+++ scripts/gen-protobuffs.sh
@@ -3,5 +3,14 @@
 set -o errexit
 set -o nounset
 
-protoc -I $GOPATH/src/github.com/googleapis/googleapis -I ./protos --elixir_out=plugins=grpc:./lib ./protos/agonex/sdk.proto
-protoc -I $GOPATH/src/github.com/googleapis/googleapis -I ./protos --elixir_out=plugins=grpc:./lib ./protos/agonex/sdk.proto $GOPATH/src/github.com/googleapis/googleapis/google/api/annotations.proto $GOPATH/src/github.com/googleapis/googleapis/google/api/http.proto
+protoc \
+  -I ./protos \
+  -I $GOPATH/src/github.com/googleapis/googleapis \
+  --elixir_out=plugins=grpc:./lib ./protos/agonex/sdk.proto
+
+protoc \
+  -I ./protos \
+  -I $GOPATH/src/github.com/googleapis/googleapis \
+  --elixir_out=plugins=grpc:./lib ./protos/agonex/sdk.proto \
+  $GOPATH/src/github.com/googleapis/googleapis/google/api/annotations.proto \
+  $GOPATH/src/github.com/googleapis/googleapis/google/api/http.proto
