2558fd0c3b280e021a845c60f11a3da8f2f03905
Shikanime Deva <deva.shikanime@protonmail.com>
2020-05-26 14:00:39 +0200

    Fix example version

--- examples/hello_agones/mix.exs
+++ examples/hello_agones/mix.exs
@@ -38,7 +38,7 @@ defmodule HelloAgones.MixProject do
   defp deps do
     [
       {:ranch, "~> 1.7"},
-      {:agonex, "~> 0.2", path: "../../"}
+      {:agonex, path: "../../"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
